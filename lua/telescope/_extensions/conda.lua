f7b6d0bbbe80321241a48ad622c9a48803025dfe
David Lewis <davidalewis00@gmail.com>
2021-12-06 18:48:47 -0500

    little fix

--- lua/telescope/_extensions/conda.lua
+++ lua/telescope/_extensions/conda.lua
@@ -1,6 +1,6 @@
 local has_telescope, telescope = pcall(require, 'telescope')
-local main = require('telescope._extensions.project.main')
-local utils = require('telescope._extensions.project.utils')
+local main = require('telescope._extensions.conda.main')
+local utils = require('telescope._extensions.conda.utils')
 
 if not has_telescope then
   error('This plugins requires nvim-telescope/telescope.nvim')
