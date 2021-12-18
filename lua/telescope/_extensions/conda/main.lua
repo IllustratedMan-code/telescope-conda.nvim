ad32310b2b2cd768c0da457cd2d6da3d611169ba
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2021-12-18 03:38:26 +0100

    added miniconda support; changed default config

--- lua/telescope/_extensions/conda/main.lua
+++ lua/telescope/_extensions/conda/main.lua
@@ -1,38 +1,56 @@
-local pickers = require "telescope.pickers"
-local finders = require "telescope.finders"
-local actions = require "telescope.actions"
-local action_state = require "telescope.actions.state"
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 local conf = require("telescope.config").values
 local scan = require("plenary.scandir")
 
-
 local M = {}
 
-local home
+local conda_path
 
 M.setup = function(setup_config)
-	home = setup_config.anaconda_path or os.getenv("HOME")
+	local home = os.getenv("HOME")
+	local viableInstallationDirs = { "/miniconda3", "/anaconda3" }
+
+	conda_path = setup_config.anaconda_path or nil -- TODO throw error on wrong path
+
+	local i = 1
+	while not conda_path and viableInstallationDirs[i] do
+		local currentPath = home .. viableInstallationDirs[i]
+		if vim.fn.isdirectory(currentPath) ~= 0 then
+			conda_path = currentPath
+		end
+		i = i + 1
+	end
+
+	if not conda_path then
+		error(
+			"Anaconda installation path not found! Please make sure Anaconda is installed or configure the correct path"
+		)
+	else
+		conda_path = vim.fn.expand(conda_path) -- To enable vars in configuration path
+	end
 end
 
 M.conda = function(opts)
-  	opts = opts or {}
-  	local conda_path = home .. "/anaconda3"
-  	local conda_env_path = home .. "/anaconda3/envs"
-  	local conda_finder = function()
-	  	local conda_envs = {}
+	opts = opts or {}
+	local conda_env_path = conda_path .. "/envs"
+	local conda_finder = function()
+		local conda_envs = {}
 		scan.scan_dir(conda_env_path, {
 			hidden = opts.hidden or false,
 			add_dirs = true,
 			depth = 1,
 			on_insert = function(entry, typ)
-			  table.insert(conda_envs, entry)
+				table.insert(conda_envs, entry)
 			end,
 		})
 
 		local conda_maker = function(entry)
 			local path_remove = function(String, Path)
 				if String == conda_path then
-					return("base")
+					return "base"
 				else
 					return string.gsub(String, Path .. "/", "")
 				end
@@ -40,46 +58,40 @@ M.conda = function(opts)
 
 			local disp = path_remove(entry, conda_env_path)
 
-			return { value = entry, display = disp, ordinal = disp}
+			return { value = entry, display = disp, ordinal = disp }
 		end
 
 		table.insert(conda_envs, conda_path)
-	  	return finders.new_table{results = conda_envs, entry_maker = conda_maker}
+		return finders.new_table({ results = conda_envs, entry_maker = conda_maker })
 	end
 
-
-  	pickers.new(opts, {
-    	prompt_title = "Select an Environment",
+	pickers.new(opts, {
+		prompt_title = "Select an Environment",
 		results_title = "Conda Environments",
-    	finder = conda_finder(),
-    	sorter = conf.generic_sorter(opts),
+		finder = conda_finder(),
+		sorter = conf.generic_sorter(opts),
 
 		attach_mappings = function(prompt_bufnr, map)
-			actions.select_default:replace(
-				function()
-					env_to_bin = function(env)
-						if env == "base" then
-							return conda_path .. "/bin"
-						else
-							return conda_env_path .. "/" .. env .. "/bin" 
-						end
+			actions.select_default:replace(function()
+				env_to_bin = function(env)
+					if env == "base" then
+						return conda_path .. "/bin"
+					else
+						return conda_env_path .. "/" .. env .. "/bin"
 					end
-					actions.close(prompt_bufnr)
-					local selection = action_state.get_selected_entry()
-					local current_env = vim.env.CONDA_DEFAULT_ENV
-					local next_env = selection["display"]
-					vim.env.CONDA_DEFAULT_ENV = next_env
-					current_anaconda = env_to_bin(current_env)
-					next_anaconda = env_to_bin(next_env)
-					vim.env.PATH = string.gsub(
-						vim.env.PATH, current_anaconda, next_anaconda
-					)
 				end
-			)
-		return true
-	end,
-
-  }):find()
+				actions.close(prompt_bufnr)
+				local selection = action_state.get_selected_entry()
+				local current_env = vim.env.CONDA_DEFAULT_ENV
+				local next_env = selection["display"]
+				vim.env.CONDA_DEFAULT_ENV = next_env
+				current_anaconda = env_to_bin(current_env)
+				next_anaconda = env_to_bin(next_env)
+				vim.env.PATH = string.gsub(vim.env.PATH, current_anaconda, next_anaconda)
+			end)
+			return true
+		end,
+	}):find()
 end
 
 return M
